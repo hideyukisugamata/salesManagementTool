03901864925625211dc9fc32f5e6f6f865792dfe
hideyuki.sugamata <hideyuki.sugamata@gmail.com>
2021-12-15 19:30:04 +0900

    customersテーブル作成

--- database/migrations/2021_12_12_065901_create_customers_table.php
+++ database/migrations/2021_12_12_065901_create_customers_table.php
@@ -14,7 +14,14 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('id');
+            $table->foreignId('user_id')->constrained();
+            $table->string('customer_name',191);
+            $table->string('customer_phone_number',191)->nullable();
+            $table->string('customer_address',191)->nullable();
+            $table->string('customer_email',191)->nullable();
+            $table->date('customer_birthday')->nullable();
+            $table->string('customer_remarks',1000)->nullable();
             $table->timestamps();
         });
     }
